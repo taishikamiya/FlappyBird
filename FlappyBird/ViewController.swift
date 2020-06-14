0ce8ff26f379a7f3e25f2673fa5ca6cc43d01998
Taishi Kamiya <t.kamiya@edu.ciid.dk>
2020-06-14 12:43:02 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by Taishi Kamiya on 2020/06/14.
+//  Copyright © 2020 taishi.kamiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
